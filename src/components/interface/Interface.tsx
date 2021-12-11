b59455e67545de087ae11eb4920cf3b77aeb9565
SanguineDerg <95940838+SanguineDerg@users.noreply.github.com>
2021-12-11 02:22:57 -0800

    Correctly disabled source link on empty slide

--- src/components/interface/Interface.tsx
+++ src/components/interface/Interface.tsx
@@ -14,7 +14,7 @@ export function Interface() {
         <div className={styles.nextSlide}>Next</div>
         <div className={styles.previousSlide}>Previous</div>
         <div className={styles.exitSlideshow} onClick={() => dispatch(switchScreen('search'))}>Close</div>
-        <a className={styles.viewSource} target="_blank" rel="noopener noreferrer" href={currentPost === null ? '#' : `https://e621.net/posts/${currentPost.id}`}>Link</a>
+        <a className={styles.viewSource} target="_blank" rel="noopener noreferrer" {...(currentPost === null ? {onClick: () => {}} : {href: `https://e621.net/posts/${currentPost.id}`})}>Link</a>
       </div>
     </div>
   );
