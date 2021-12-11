35258b450df3a3c0a2b9794983a1b0cafe05c842
SanguineDerg <95940838+SanguineDerg@users.noreply.github.com>
2021-12-11 00:02:17 -0800

    Removed example code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, createAsyncThunk, ThunkAction, Action, AsyncThunkPayloadCreator, AsyncThunkOptions, AsyncThunk } from '@reduxjs/toolkit';
 import postsReducer from '../features/posts/postsSlice';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     posts: postsReducer,
   },
 });
