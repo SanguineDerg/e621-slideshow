83d4ee699ef91f1a005c0fb8b0c25b4cdb7ebb28
SanguineDerg <95940838+SanguineDerg@users.noreply.github.com>
2021-12-11 03:26:46 -0800

    Added message for non-image posts

--- src/components/display/Display.tsx
+++ src/components/display/Display.tsx
@@ -1,13 +1,45 @@
 import { useSelector } from 'react-redux';
+import { Post } from '../../api/e621/interfaces/posts';
 import { selectCurrentSlideshowPost } from '../../slices/postsSlice';
 import styles from './Display.module.css';
+
+type Filetype = 'image' | 'video' | 'flash';
+
+function getFiletype(post: Post | null) {
+  if (post === null) return null;
+  switch (post.file.ext) {
+    case 'jpg':
+    case 'png':
+    case 'gif':
+      return 'image';
+    case 'webm':
+      return 'video';
+    case 'swf':
+      return 'flash';
+    default:
+      return null;
+  }
+}
 
 export function Display() {
   const currentPost = useSelector(selectCurrentSlideshowPost);
 
+  const currentFiletype = getFiletype(currentPost);
+
   return (
     <div className={styles.displayContainer}>
-      <img className={styles.image} src={currentPost?.file.url} alt="" />
+      {currentFiletype === 'image' && (
+        <img className={styles.image} src={currentPost?.file.url} alt="" />
+      )}
+      {currentFiletype === 'video' && (
+        <div className={styles.text}><span>Video</span></div>
+      )}
+      {currentFiletype === 'flash' && (
+        <div className={styles.text}><span>Flash</span></div>
+      )}
+      {currentFiletype === null && currentPost !== null && (
+        <div className={styles.text}><span>Invalid File Extension: {currentPost?.file.ext}</span></div>
+      )}
     </div>
   );
 }
