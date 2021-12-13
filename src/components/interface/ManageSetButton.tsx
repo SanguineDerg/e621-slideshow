78b088a0c64b757b8829e87d1bd5ef624509badf
SanguineDerg <95940838+SanguineDerg@users.noreply.github.com>
2021-12-12 16:51:02 -0800

    Added keybinds for adding/removing post from set

--- src/components/interface/ManageSetButton.tsx
+++ src/components/interface/ManageSetButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { selectCurrentSlideshowPostId, selectIsCurrentPostInSet } from "../../slices/postsSlice";
 import { addCurrentPostToSet, removeCurrentPostFromSet, selectUpdateSetStatus, selectWorkingSet } from "../../slices/setSlice";
@@ -44,6 +44,34 @@ export default function ManageSetButton() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [updateSetState, isPostInSet]);
 
+  const tryAddToSet = useCallback(() => {
+    if (currentPostId === null || updateSetState === 'working' || isPostInSet) return;
+    dispatch(addCurrentPostToSet(currentPostId));
+  }, [dispatch, currentPostId, updateSetState, isPostInSet]);
+
+  const tryRemoveFromSet = useCallback(() => {
+    if (currentPostId === null || updateSetState === 'working' || !isPostInSet) return;
+    dispatch(removeCurrentPostFromSet(currentPostId));
+  }, [dispatch, currentPostId, updateSetState, isPostInSet]);
+
+  const keydownHandler = useCallback((e: KeyboardEvent) => {
+    if (e.repeat) return;
+    switch (e.key) {
+      case "ArrowUp":
+        tryAddToSet();
+        return;
+      case "ArrowDown":
+        tryRemoveFromSet();
+        return;
+      default: return;
+    }
+  }, [tryAddToSet, tryRemoveFromSet]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", keydownHandler, true);
+    return () => window.removeEventListener("keydown", keydownHandler, true);
+  }, [keydownHandler]);
+  
   if (currentPostId === null || workingSet === null) return null;
 
   const handleClick = () => {
