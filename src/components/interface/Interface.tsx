8d7d14b58e63e2967f41f352a3733aaa8300450a
SanguineDerg <95940838+SanguineDerg@users.noreply.github.com>
2021-12-12 20:21:41 -0800

    Added keybinds for next/previous slide

--- src/components/interface/Interface.tsx
+++ src/components/interface/Interface.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { nextSlideAndPrefetch, previousSlideAndPrefetch, selectCurrentSlideshowPost } from '../../slices/postsSlice';
 import { switchScreen } from '../../slices/viewSlice';
@@ -8,12 +9,38 @@ export function Interface() {
   const dispatch = useAppDispatch();
 
   const currentPost = useAppSelector(selectCurrentSlideshowPost);
+  
+  const nextSlide = useCallback(() => {
+    dispatch(nextSlideAndPrefetch());
+  }, [dispatch]);
+  
+  const prevSlide = useCallback(() => {
+    dispatch(previousSlideAndPrefetch());
+  }, [dispatch]);
+
+  const keydownHandler = useCallback((e: KeyboardEvent) => {
+    if (e.repeat) return;
+    switch (e.key) {
+      case "ArrowLeft":
+        prevSlide();
+        return;
+      case "ArrowRight":
+        nextSlide();
+        return;
+      default: return;
+    }
+  }, [nextSlide, prevSlide]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", keydownHandler, true);
+    return () => window.removeEventListener("keydown", keydownHandler, true);
+  }, [keydownHandler]);
 
   return (
     <div className={styles.interfaceContainer}>
       <div className={styles.mainControls}>
-        <div className={styles.nextSlide} onClick={() => dispatch(nextSlideAndPrefetch())}>Next</div>
-        <div className={styles.previousSlide} onClick={() => dispatch(previousSlideAndPrefetch())}>Previous</div>
+        <div className={styles.nextSlide} onClick={nextSlide}>Next</div>
+        <div className={styles.previousSlide} onClick={prevSlide}>Previous</div>
         <div className={styles.exitSlideshow} onClick={() => dispatch(switchScreen('search'))}>Close</div>
         <a className={styles.viewSource} target="_blank" rel="noopener noreferrer" {...(currentPost === null ? {onClick: () => {}} : {href: `https://e621.net/posts/${currentPost.id}`})}>Link</a>
       </div>
