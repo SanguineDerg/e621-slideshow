a5f381a8a7339432ccb894d7b4f5a6de0f03aded
SanguineDerg <SanguineFluff@gmail.com>
2025-09-23 18:16:56 -0700

    Fixed mp4 posts and samples sometimes breaking

--- src/api/e621/interfaces/posts.ts
+++ src/api/e621/interfaces/posts.ts
@@ -27,7 +27,7 @@ export interface Post {
 export interface PostFile {
   width: number;
   height: number;
-  ext: 'jpg' | 'png' | 'gif' | 'webm' | 'swf';
+  ext: 'jpg' | 'png' | 'gif' | 'webm' | 'mp4'| 'swf';
   size: number;
   md5: string;
   url: string;
