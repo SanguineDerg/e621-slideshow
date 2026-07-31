fee2cb4812d71affea0f9531d5d7f682694aa850
Sanguine <95940838+SanguineDerg@users.noreply.github.com>
2026-07-31 01:55:04 -0700

    Fix URLs in some situations

--- src/api/e621/posts.ts
+++ src/api/e621/posts.ts
@@ -37,28 +37,35 @@ const getCurrentImageSite = () => {
   return (account === null || account.site === DEFAULT_SITE) ? 'https://static1.e621.net' : account.site;
 }
 
+/**
+ * Converts a possibly relative URL from the API to an absolute URL.
+ */
+const absoluteUrlForCurrentSite = (relativeUrl: string) => {
+  const imageSite = getCurrentImageSite();
+  return new URL(relativeUrl, imageSite).href;
+}
+
 export const getPostImageUrl = (post: Post) => {
   const imageDisplaySize = readImageDisplaySize();
   switch (imageDisplaySize) {
     case 'sample':
     default:
-      return post.sample.url || getPostImageBypassUrl(post);
+      return absoluteUrlForCurrentSite(post.sample.url || getPostImageBypassUrl(post));
     case 'full':
-      return post.file.url || getPostImageBypassUrl(post);
+      return absoluteUrlForCurrentSite(post.file.url || getPostImageBypassUrl(post));
   }
 }
 
 const getPostImageBypassUrl = (post: Post) => {
   const imageDisplaySize = readImageDisplaySize();
   const md5 = post.file.md5;
-  const imageSite = getCurrentImageSite();
   switch (imageDisplaySize) {
     case 'sample':
     default:
-      if (post.sample.has) return `${imageSite}/data/sample/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.jpg`;
-      return `${imageSite}/data/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.${post.file.ext}`; // Fallback to full
+      if (post.sample.has) return `/data/sample/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.jpg`;
+      return `/data/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.${post.file.ext}`; // Fallback to full
     case 'full':
-      return `${imageSite}/data/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.${post.file.ext}`;
+      return `/data/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.${post.file.ext}`;
   }
 }
 
@@ -73,52 +80,60 @@ const getVideoTypeIndex = (type: VideoDisplayType) => {
 }
 
 export const getPostVideoUrl = (post: Post) => {
-  if (post.file.url === null) return getPostVideoBypassUrl(post);
+  if (post.file.url === null) return absoluteUrlForCurrentSite(getPostVideoBypassUrl(post));
   const videoDisplaySize = readVideoDisplaySize();
   const videoTypeIndex = getVideoTypeIndex(readVideoDisplayType());
   switch (videoDisplaySize) {
     case '480p':
-      if (!!post.sample.alternates['480p']) return post.sample.alternates['480p']?.urls[videoTypeIndex] as string;
-      if (!!post.sample.alternates.original && videoTypeIndex !== 0) return post.sample.alternates.original?.urls[videoTypeIndex] as string;
-      return post.file.url;
+      if (!!post.sample.alternates['480p']) {
+        return absoluteUrlForCurrentSite(post.sample.alternates['480p']?.urls[videoTypeIndex] as string);
+      }
+      if (!!post.sample.alternates.original && videoTypeIndex !== 0) {
+        return absoluteUrlForCurrentSite(post.sample.alternates.original?.urls[videoTypeIndex] as string);
+      }
+      return absoluteUrlForCurrentSite(post.file.url);
     case '720p':
     default:
-      if (!!post.sample.alternates['720p']) return post.sample.alternates['720p']?.urls[videoTypeIndex] as string;
-      if (!!post.sample.alternates.original && videoTypeIndex !== 0) return post.sample.alternates.original?.urls[videoTypeIndex] as string;
-      return post.file.url;
+      if (!!post.sample.alternates['720p']) {
+        return absoluteUrlForCurrentSite(post.sample.alternates['720p']?.urls[videoTypeIndex] as string);
+      }
+      if (!!post.sample.alternates.original && videoTypeIndex !== 0) {
+        return absoluteUrlForCurrentSite(post.sample.alternates.original?.urls[videoTypeIndex] as string);
+      }
+      return absoluteUrlForCurrentSite(post.file.url);
     case 'full':
-      if (!!post.sample.alternates.original && videoTypeIndex !== 0) return post.sample.alternates.original?.urls[videoTypeIndex] as string;
-      return post.file.url;
+      if (!!post.sample.alternates.original && videoTypeIndex !== 0) {
+        return absoluteUrlForCurrentSite(post.sample.alternates.original?.urls[videoTypeIndex] as string);
+      }
+      return absoluteUrlForCurrentSite(post.file.url);
   }
 }
 
 const getPostVideoBypassUrl = (post: Post) => {
   const videoDisplaySize = readVideoDisplaySize();
   const md5 = post.file.md5;
-  const imageSite = getCurrentImageSite();
   const videoDisplayType = readVideoDisplayType();
   switch (videoDisplaySize) {
     case '480p':
-      if (!!post.sample.alternates['480p']) return `${imageSite}/data/sample/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}_480p.${videoDisplayType}`;
-      return `${imageSite}/data/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.${videoDisplayType}`;
+      if (!!post.sample.alternates['480p']) return `/data/sample/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}_480p.${videoDisplayType}`;
+      return `/data/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.${videoDisplayType}`;
     case '720p':
     default:
-      if (!!post.sample.alternates['720p']) return `${imageSite}/data/sample/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}_720p.${videoDisplayType}`;
-      return `${imageSite}/data/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.${videoDisplayType}`;
+      if (!!post.sample.alternates['720p']) return `/data/sample/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}_720p.${videoDisplayType}`;
+      return `/data/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.${videoDisplayType}`;
     case 'full':
-      return `${imageSite}/data/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.${videoDisplayType}`;
+      return `/data/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.${videoDisplayType}`;
   }
 }
 
 export const getPostVideoPreviewUrl = (post: Post) => {
-  if (post.sample.url === null) return getPostVideoPreviewBypassUrl(post);
-  return post.sample.url;
+  if (post.sample.url === null) return absoluteUrlForCurrentSite(getPostVideoPreviewBypassUrl(post));
+  return absoluteUrlForCurrentSite(post.sample.url);
 }
 
 const getPostVideoPreviewBypassUrl = (post: Post) => {
   const md5 = post.file.md5;
-  const imageSite = getCurrentImageSite();
-  return `${imageSite}/data/sample/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.jpg`;
+  return `/data/sample/${md5.substring(0, 2)}/${md5.substring(2, 4)}/${md5}.jpg`;
 }
 
 export default PostAPI
